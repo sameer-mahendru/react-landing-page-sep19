c33c11ac0593cdccf92ad9528efbc181620c3495
danyentezari <dany.entezari@gmail.com>
2019-09-21 15:38:24 +0400

    Moved components around.

--- src/NavBar.js
+++ src/NavBar.js
@@ -30,15 +30,17 @@ const LoginGroup = () => {
 const NavBar = (prop) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a href="#">{prop.logo}</a>
-            
-            <LoginGroup />
+            <div>
+                <a href="#">{prop.logo}</a>       
+            </div>
 
             <div className="btn-group">
               <button className="btn btn-primary">Home</button>
               <button className="btn btn-primary">About</button>
               <button className="btn btn-primary">Contact</button>
             </div>
+
+            <LoginGroup />
         </nav>
     )
 }
