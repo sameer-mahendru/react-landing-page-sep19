1424ecb9847d5acf8230b4599549c31133d726b1
danyentezari <dany.entezari@gmail.com>
2019-09-21 12:51:16 +0400

    Introduced useState().

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from './utils';
 
 const Jumbotron = (prop) => {
+
+    const [state, setState] = useState(
+        { emailValid: null }
+    );
+
+    // Above code is short for the following: 
+    // const reactArray = useState(false); // [false, setSubscribed]
+    // const subscribed = reactArray[0];
+    // const setSubscribed = reactArray[1]
+
+    let emailField;
+
+    const subsribeUser = () => {
+        if(!validateEmail(emailField.value)) {
+            setState({ emailValid: 'invalid' })
+        } else {
+            setState({ emailValid: 'valid' })
+        }
+    }
+
     return (
         <div className="jumbotron">
             <div className="container">
@@ -11,14 +32,37 @@ const Jumbotron = (prop) => {
 
                 <div className="form-row align-items-center">
                     <div className="col-auto my-1 col-sm-4">
-                        <input type="text" className="form-control"/>
+                        <input 
+                        ref={ (inputField)=>{ emailField = inputField} }
+                        placeholder="Enter email address" type="text" className="form-control"/>
                     </div>
                     <div className="col-auto my-1">
+                        
+                    {
+                        state.emailValid !== "valid" &&
                         <button 
+                        onClick={subsribeUser}
                         type="submit"
                         className="btn btn-primary">Subscribe</button>
+                    }
+
+
                     </div>
                 </div>
+
+                {
+                    state.emailValid === "valid" && 
+                    <div className="alert alert-success" role="alert">
+                        You've been subscribed successfully!
+                    </div>
+                }
+
+                {
+                    state.emailValid === "invalid" && 
+                    <div className="alert alert-danger" role="alert">
+                        Please enter a valid email address.
+                    </div>
+                }
             </div>
         </div>
     )
