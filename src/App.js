e58b3ea0415337760f8b19f7e6591c5bf00940d4
danyentezari <dany.entezari@gmail.com>
2019-09-23 20:37:32 +0400

    Rendering components with arrays.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import ColorButton from './ColorButton';
 import Jumbotron from './Jumbotron';
 import './App.css';
 
-export const AppContext = createContext();
+export const AppContext = createContext(); // component
 
 const App = () => {
 
   const [globalState, setGlobalState] = useState(
       { 
           loggedIn: false,
-          users: []
+          users: [
+            'Jim',
+            'Jane',
+            'Mohammad',
+            'Mary'
+          ]
       }
   );
 
@@ -35,7 +40,21 @@ const App = () => {
             description="This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
           />
 
-          <ColorButton />
+          <ul>
+          { 
+            
+            globalState.users.map(
+              (name) => {
+                return(
+                  <li>
+                    { `My name is ${name}` }
+                  </li>
+                )
+              }
+            ) 
+          }
+          </ul>
+
       </div>
     </AppContext.Provider>
   );
